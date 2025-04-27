a8e4ab9e71f1b96de22320905c16f16e098ccf85
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2025-04-27 18:16:52 -0300

    fix payload-area to be a property; next=fix height to be 100% of parent div

--- front/src/app/components/payload-area/payload-area.component.ts
+++ front/src/app/components/payload-area/payload-area.component.ts
@@ -1,21 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-payload-area',
-  imports: [FormsModule],
-  templateUrl: './payload-area.component.html',
-  styleUrl: './payload-area.component.css'
+  selector: '[app-payload-area]', // seletor de atributo
+  standalone: true,
+  template: '', // sem HTML aqui!
+  styleUrls: ['./payload-area.component.css'],
+  host: {
+    '[attr.data-payload]': 'payloadId',
+    '[name]': 'payloadName',
+    'class': 'payload-input',
+    '[placeholder]': '"Payload list " + payloadId',
+    '[ngModel]': 'payloadText',
+    '(ngModelChange)': 'payloadText = $event'
+  }
 })
-export class PayloadAreaComponent implements OnInit {
-
-  @Input() payloadId! : number;
-  payloadName!: string;
-  payloadText: string = "";
+export class PayloadAreaComponent {
+  @Input() payloadId!: number;
+  @Input() payloadName!: string;
+  @Input() payloadText!: string;
 
   ngOnInit(): void {
     this.payloadName = "payload" + this.payloadId;
     console.log(this.payloadName);
   }
-
 }
+
